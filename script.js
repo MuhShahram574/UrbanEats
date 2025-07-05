094a55349dadb6b225e8790f6de889c96b2c4202
MuhShahram <mshahram574@gmail.com>
2025-07-05 23:28:26 +0500

    update in feat-home-page-ui

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const header = document.querySelector("header");
 const mobileNavBtn = document.querySelector(".mobile-nav-btn");
 const navLinks = document.querySelectorAll(".nav-links");
 const heroSection = document.querySelector(".heros-section");
+const hamBurger = document.querySelector(".mobile-nav-btn");
+const aside = document.querySelector("aside");
+const crossBtnBox = document.querySelector(".cross-btn-box");
+
+function showNavbarMenu() {
+  hamBurger.addEventListener("click", () => {
+    hamBurger.classList.add("hidden");
+    aside.classList.remove("hidden");
+    aside.classList.add("fade-left");
+    aside.classList.remove("fade-right");
+  });
+}
+function hideNavbarMenu() {
+  crossBtnBox.addEventListener("click", () => {
+    hamBurger.classList.remove("hidden");
+    aside.classList.add("fade-right");
+    aside.classList.remove("fade-left");
+  });
+}
+
+showNavbarMenu();
+hideNavbarMenu();
 
 // Toggle mobile menu
 mobileNavBtn.addEventListener("click", () => {
@@ -29,20 +51,18 @@ window.addEventListener("scroll", () => {
   const scrollUp = document.querySelector(".scroll-up");
   if (window.scrollY >= heroBottom) {
     header.classList.add("scrolled");
-    scrollUp.classList.remove("scroll-remove");
   } else {
     header.classList.remove("scrolled");
-    scrollUp.classList.add("scroll-remove");
   }
 });
 
 // JavaScript for the preloader
-document.addEventListener("DOMContentLoaded", () => {
-  const preloader = document.querySelector(".preloader");
-  const content = document.querySelector(".content");
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      preloader.classList.add("hidden");
-    }, 500);
-  });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   const preloader = document.querySelector(".preloader");
+//   const content = document.querySelector(".content");
+//   window.addEventListener("load", () => {
+//     setTimeout(() => {
+//       preloader.classList.add("hidden");
+//     }, 500);
+//   });
+// });
